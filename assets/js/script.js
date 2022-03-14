5828debd7f57b2e1515e55582dd6c6d5b563cad7
Jonnywdev <jonnywhitt19@gmail.com>
2022-03-14 17:30:53 +0000

    add back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
-var container = document.getElementById('cloud-container');
-var image1 = document.getElementsById('one');
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-// update position of square 1 and square 2 when scroll event fires.
-window.addEventListener('scroll', function() {
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-
-  image1.style = scrollPercent*window.innerWidth + 'px';
-});
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
